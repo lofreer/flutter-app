031b6552ea4228f3121b9408fd30be8c980e04bc
lofreer <lofreer@gmail.com>
2020-09-29 16:30:24 +0800

    个人中心页；

---
+++ lib/provider/watch_history_page_model.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:demo/model/issue_model.dart';
+import 'package:demo/repository/history_repository.dart';
+
+class WatchHistoryPageModel with ChangeNotifier {
+  List<Data> itemList = [];
+  List<String> watchList = [];
+
+  void loadData() async {
+    watchList = await HistoryRepository.loadHistoryData();
+    if (watchList != null) {
+      var list = watchList.map((value) {
+        return Data.fromJson(json.decode(value));
+      }).toList();
+      itemList = list;
+      notifyListeners();
+    }
+  }
+
+  void remove(int index) async {
+    watchList.removeAt(index);
+    HistoryRepository.saveHistoryData(watchList);
+    loadData();
+  }
+}
